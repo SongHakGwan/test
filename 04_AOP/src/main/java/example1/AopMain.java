a2ff24c6bc51e65a80e223dabd623046c0bf25a9
Song <shg7248@gmail.com>
2022-08-19 19:05:36 +0900

    dfsdsfdfs

---
+++ 04_AOP/src/main/java/example1/AopMain.java
@@ -0,0 +1,12 @@
+package example1;
+
+public class AopMain {
+	public static void main(String[] args) {
+		
+		Board board = new Board();
+		board.board();
+		
+		Order order = new Order();
+		order.order();
+	}
+}
---
+++ 04_AOP/src/main/java/example3/BoardImpl.java
@@ -0,0 +1,12 @@
+package example3;
+
+public class BoardImpl implements Board{
+
+	@Override
+	public void board() {
+		
+		String msg = "게시물 등록";
+		System.out.println(msg + "하기"); // 핵심기능
+	}
+	
+}
